315099fc403c58b8c9d74bcd90eb02820d63fa8b
Masoud KA <git@masoudka.com>
2024-10-09 01:37:10 +0000

    Refactor: Update dataset directory paths and import statements in config.py

--- math2latex/config/config.py
+++ math2latex/config/config.py
@@ -1,64 +1,52 @@
-class Config:
-    def __init__(self, dictionary):
-        for key, value in dictionary.items():
-            setattr(self, key, Config(value) if isinstance(value, dict) else value)
+
+from .utils import Config
+
 
 dataset_dir = 'dataset'
 
 all_configurations = {
-    'train_dataset': {
-        'root': dataset_dir,
-        'images_folder': 'formula_images_processed',
-        'label_file': 'im2latex_formulas.norm.processed.lst',
-        'data_filter': 'im2latex_train_filter.lst',
-        'transform': 'train'
+    'datamodule': {
+        'train_dataset_root': dataset_dir,
+        'train_dataset_images_folder': 'formula_images_processed',
+        'train_dataset_label_file': 'im2latex_formulas.norm.processed.lst',
+        'train_dataset_data_filter': 'im2latex_train_filter.lst',
+        'train_dataset_transform': 'train',
+        'val_dataset_root': dataset_dir,
+        'val_dataset_images_folder': 'formula_images_processed',
+        'val_dataset_label_file': 'im2latex_formulas.norm.processed.lst',
+        'val_dataset_data_filter': 'im2latex_validate_filter.lst',
+        'val_dataset_transform': 'test',
+        'test_dataset_root': dataset_dir,
+        'test_dataset_images_folder': 'formula_images_processed',
+        'test_dataset_label_file': 'im2latex_formulas.norm.processed.lst',
+        'test_dataset_data_filter': 'im2latex_test_filter.lst',
+        'test_dataset_transform': 'test',
+        'train_dataloader_batch_size': 64,
+        'train_dataloader_num_workers': 32,
+        'val_dataloader_batch_size': 64,
+        'val_dataloader_num_workers': 32,
+        'test_dataloader_batch_size': 64,
+        'test_dataloader_num_workers': 32
     },
-    'val_dataset': {
-        'root': dataset_dir,
-        'images_folder': 'formula_images_processed',
-        'label_file': 'im2latex_formulas.norm.processed.lst',
-        'data_filter': 'im2latex_validate_filter.lst',
-        'transform': 'test'
-    },
-    'test_dataset': {
-        'root': dataset_dir,
-        'images_folder': 'formula_images_processed',
-        'label_file': 'im2latex_formulas.norm.processed.lst',
-        'data_filter': 'im2latex_test_filter.lst',
-        'transform': 'test'
-    },
-    'train_dataloader': {
-        'batch_size': 64,
-        'num_workers': 32
-    },
-    'val_dataloader': {
-        'batch_size': 64,
-        'num_workers': 32
-    },
-    'test_dataloader': {
-        'batch_size': 64,
-        'num_workers': 32
-    },
-    'model': {
+    'litmodel': {
         'd_model': 128,
         'num_heads': 4,
         'num_decoder_layers': 3,
         'dim_feedforward': 256,
         'dropout': 0.3,
         'num_classes': 462,
-        'max_len': 150
-    },
-    'optimizer': {
+        'max_len': 150,
         'lr': 0.001,
         'weight_decay': 0.0001,
         'milestones': [10],
         'gamma': 0.5
     },
     'trainer': {
-        'epochs': 100,
-        'accelerator': 'gpu',
-        'batch_size': 32,
-        'learning_rate' : 0.001
+        'max_epochs': 200,
+        'accelerator': 'auto',
+        'strategy': 'auto',
+        'enable_progress_bar': True,
+        'check_val_every_n_epoch': 1,
     }
 }
 
