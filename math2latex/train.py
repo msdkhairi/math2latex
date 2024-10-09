a1cb21ec3bb714e8b755aac51f58c9cb4a52e9a5
Masoud KA <git@masoudka.com>
2024-10-09 01:37:30 +0000

    Refactor: Update import statements and file paths in train.py

--- math2latex/train.py
+++ math2latex/train.py
@@ -1,36 +1,30 @@
 import torchtext; torchtext.disable_torchtext_deprecation_warning()
 
-from utils import LitMathToLatex, LitMathToLatexDataModule
-from config import config
+# from utils import LitMathToLatex, LitMathToLatexDataModule
+from runner import LitMathToLatex, LitMathToLatexDataModule
+from config import config as cfg
 
 from lightning import Trainer
 from lightning.pytorch.loggers import TensorBoardLogger
 from lightning.pytorch.tuner import Tuner
 
 def main():
-    # runner = Runner(config)
-    # runner.train()
 
-    datamodule = LitMathToLatexDataModule(config)
-    model = LitMathToLatex(config)
+    datamodule = LitMathToLatexDataModule(**cfg.datamodule.to_dict())
+    model = LitMathToLatex(**cfg.litmodel.to_dict())
 
     logger = TensorBoardLogger(save_dir="logs/", name="math2latex")
 
     trainer = Trainer(
         logger=logger,
-        max_epochs=config.trainer.epochs,
-        accelerator=config.trainer.accelerator,
-        enable_progress_bar=True
+        **cfg.trainer.to_dict()
     )
 
     # tuner = Tuner(trainer=trainer)
-
     # tuner.scale_batch_size(model, datamodule=datamodule, mode="power")
     # tuner.lr_find(model, datamodule=datamodule)
 
     trainer.fit(model, datamodule=datamodule)
-
-
 
 if __name__ == '__main__':
     main()
